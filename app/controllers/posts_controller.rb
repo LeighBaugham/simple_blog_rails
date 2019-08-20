9072c833e26980fe729df0f979aceb1499d1903d
Leigh Baugham <leigh.baugham@gmail.com>
2019-08-20 13:59:46 -0400

    create posts, display posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,27 @@
 class PostsController < ApplicationController
 
     def index
-
+        @posts = Post.all
     end
 
+
     def show
+        @post = Post.find(params[:id])
     end
 
     def new
     end
 
-    def edit
+    def create
+        #render plain: params[:post].inspect
+        @post = Post.new(post_params)
+        @post.save
+        redirect_to @post
     end
 
-    def destroy
+
+    private def post_params
+        params.require(:post).permit(:title, :body)
     end
-    
+
 end
